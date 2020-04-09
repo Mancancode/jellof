def2c278c44891e5e5adeb99a1ad8233abac1e3c
mancancode <madebymaniq@live.com>
2020-04-09 22:20:09 +0100

    changed all folder

---
+++ footbalStar/README.md
@@ -0,0 +1,2 @@
+# PepoStriker
+A fun website built with css html and vanilla js with love from me to my cousin pope
--- footbalStar/app.js
+++ footbalStar/app.js
@@ -1,7 +1,15 @@
 const hamburger = document.getElementById('hamburgerBtn');
 const nav = document.getElementById('nav');
 
-hamburgerBtn.addEventListener('click',( )=>{
-    nav.classList.toggle('open');
+let navOpen = false
 
-    });
+hamburgerBtn.addEventListener('click',( )=>{
+    navOpen = !navOpen;
+    if(navOpen){
+        nav.style.display = 'flex';
+        nav.classList.add('open');
+    } else {
+        nav.style.display = 'none';
+        nav.classList.remove('open');
+    }
+});
